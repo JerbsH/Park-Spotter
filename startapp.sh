3d8c1cd6a096c8ce991efa6af8bc5c4ba04476d0
Jere <101744771+JerbsH@users.noreply.github.com>
2024-04-05 16:05:24 +0300

    FIX: script fix

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-pip install -r ./backend/requirements.txt;
-python3 ./backend/streamyolo.py;
+
+# Update and upgrade system packages
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+# Install pip
+sudo apt-get install python3-pip -y
+
+# Install required python packages
+pip install -r ./backend/requirements.txt
+
+# Run the application
+python3 ./backend/streamyolo.py
