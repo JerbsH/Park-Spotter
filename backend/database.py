dd26779a47c5ecdcb3849129cab5622bfadf2deb
MiikaJok <miikajok@metropolia.fi>
2024-04-05 19:30:06 +0300

    FIX:database gitignore removed, secure data moved to .env

---
+++ backend/database.py
@@ -0,0 +1,48 @@
+"""The code connects to a MySQL database
+and fetches all the rows from a table."""
+
+import os
+import mysql.connector
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def connect_to_db():
+    cnx = mysql.connector.connect(
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASS'),
+        database=os.getenv('DB_NAME')
+    )
+    return cnx
+
+
+def fetch_available_spots():
+    cnx = connect_to_db()
+    try:
+        cursor = cnx.cursor()
+        query = "SELECT PARKSPOTS FROM AVAILABLE_SPOTS"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        if result is not None:
+            print(f"Fetched {result[0]} available spots")  # Debugging print statement
+            return result[0]
+        else:
+            print("No available spots found")
+            return 0
+    except mysql.connector.Error as err:
+        print(f"Something went wrong: {err}")
+    finally:
+        cnx.close()
+
+def store_free_spots(free_spots):
+    cnx = connect_to_db()
+    try:
+        cursor = cnx.cursor()
+        query = "UPDATE AVAILABLE_SPOTS SET PARKSPOTS = %s WHERE id = %s"
+        cursor.execute(query, (free_spots, 1))  # Provide the id parameter
+        cnx.commit()
+    except mysql.connector.Error as err:
+        print(f"Something went wrong: {err}")
+    finally:
+        cnx.close()
