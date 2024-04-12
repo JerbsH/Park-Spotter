78067f8de9c607a1a6fb17f65e6454957c027cf6
MiikaJok <miikajok@metropolia.fi>
2024-04-12 15:31:43 +0300

    FIX:backend-frontend connection working now

--- backend/flaskserver.py
+++ backend/flaskserver.py
@@ -2,7 +2,6 @@ import logging
 from flask import Flask, jsonify
 from flask_cors import CORS
 
-
 try:
     from database import fetch_available_spots, fetch_available_handicap_spots
 except ImportError:
@@ -22,6 +21,7 @@ def get_free_spots():
         free_spots = fetch_available_spots()
         return jsonify({'free_spots': free_spots}), 200
     except Exception as e:
+        logging.exception("Error fetching free spots: %s", str(e))
         return jsonify({'error': str(e)}), 500
 
 @app.route('/free_handicap_spots', methods=['GET'])
@@ -33,6 +33,7 @@ def get_free_handicap_spots():
         free_handicap_spots = fetch_available_handicap_spots()
         return jsonify({'free_handicap_spots': free_handicap_spots}), 200
     except Exception as e:
+        logging.exception("Error fetching free handicap spots: %s", str(e))
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
