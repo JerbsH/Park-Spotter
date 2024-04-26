39f0cf1104cff5c2108cc51b32a4ea1e1a19478d
Jere <101744771+JerbsH@users.noreply.github.com>
2024-04-26 16:51:05 +0300

    FIX: static folder

---
+++ backend/static/webscripts.js
@@ -0,0 +1,27 @@
+function sendData() {
+  var parking = document.getElementById("number1").value;
+  var accPark = document.getElementById("number2").value;
+
+  sessionStorage.setItem("parking", parking);
+  sessionStorage.setItem("accPark", accPark);
+
+  window.location.href='draw.html';
+}
+
+function showDraw() {
+  document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve the values from session storage
+    var parking = sessionStorage.getItem("parking");
+    var accPark = sessionStorage.getItem("accPark");
+
+    // Display the values in the respective divs
+    document.getElementById("park").innerHTML = parking;
+    document.getElementById("acc").innerHTML = accPark;
+
+    fetch('/run-opencv').then(response => {
+      console.log('opencv.py executed successfully');
+    }).catch(error => {
+      console.error('Error executing opencv.py:', error);
+    });
+});
+}
