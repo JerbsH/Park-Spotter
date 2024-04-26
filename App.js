7b34fe55e91bf89bbe8e9d07ad14cc6c242bee4a
MiikaJok <miikajok@metropolia.fi>
2024-04-26 13:20:29 +0300

    ADD:test comment for deploy

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ import {
 } from './frontend/locationservice';
 
 initializeApp(firebaseConfig);
-
+// test comment
 const App = () => {
   const [spots, setSpots] = useState(0);
   const [handicapSpots, setHandicapSpots] = useState(0);
