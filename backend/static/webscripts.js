c206a50ca7ea81222d7f6bb4d35c8ff7cc0f00fc
MiikaJok <miikajok@metropolia.fi>
2024-05-05 20:10:43 +0300

    ADD:saving image to database, fetching it for python program

--- backend/static/webscripts.js
+++ backend/static/webscripts.js
@@ -1,28 +1,27 @@
 function sendData() {
   const parking = document.getElementById('number1').value;
   const accPark = document.getElementById('number2').value;
+  const imagePicker = document.getElementById('imagePicker').files[0];
 
-  sessionStorage.setItem('parking', parking);
-  sessionStorage.setItem('accPark', accPark);
+  const formData = new FormData();
+  formData.append('image', imagePicker);
+  formData.append('parking', parking);
+  formData.append('accPark', accPark);
 
   fetch('/save_spots', {
     method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      parking: parking,
-      accPark: accPark,
-    }),
+    body: formData,
   })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log('Success:', data);
-      window.location.href = './static/draw.html';
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Success:', data);
+    sessionStorage.setItem('parking', parking);
+    sessionStorage.setItem('accPark', accPark);
+    window.location.href = './static/draw.html';
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 }
 
 function showDraw() {
@@ -44,3 +43,22 @@ function showDraw() {
       });
   });
 }
+
+function checkImageSize(input) {
+  var file = input.files[0];
+  var img = new Image();
+  var reader = new FileReader();
+
+  reader.onload = function(e) {
+    img.onload = function() {
+      if (img.width !== 3840 || img.height !== 2160) {
+        alert('Please select an image of size 3840x2160.');
+        input.value = '';
+      }
+    };
+    img.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+}
+
